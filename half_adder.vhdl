b680837f6ca6d38ce17079ee8a9cb401b99b27d4
cloner <cloner010103@yahoo.com>
2025-03-09 11:58:47 +0330

    Update half_adder.vhdl

--- half_adder.vhdl
+++ half_adder.vhdl
@@ -72,7 +72,7 @@ end adder8bit;
 
 architecture first of adder8bit is
   component cloner
-    port(A,B,Cin:in bit;Sum,carry_out:out bit)
+    port (A,B,Cin:in bit;S,Cout:out bit)
   end component;    
 for all : cloner use entity work.full_adder(madar);
   signal x7,x6,x5,x4,x3,x2,x2,x1:bit;
