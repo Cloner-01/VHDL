5442b3e6d31e0915a52b09839968e20716f5797a
cloner <cloner010103@yahoo.com>
2025-04-14 01:38:20 +0330

    Update half_adder.vhdl

--- half_adder.vhdl
+++ half_adder.vhdl
@@ -134,7 +134,7 @@ begin
   mux3 : cloner port map (A(3),B(3),C(3),D(3),S1,S0,muxout(3));
   mux2 : cloner port map (A(2),B(2),C(2),D(2),S1,S0,muxout(2));
   mux1 : cloner port map (A(1),B(1),C(1),D(1),S1,S0,muxout(1));
-  mux0 : cloner port map (A(0),B(0),C(0),D(0),S1,S0,muxout(1));
+  mux0 : cloner port map (A(0),B(0),C(0),D(0),S1,S0,muxout(0));
 end first ;
 
 #AU_8bit
